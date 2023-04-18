3f27971e4bd7b69adfc6f960364bdde652831d79
Nahuel Brugnoni <nahubrugnoni@gmail.com>
2023-04-17 21:33:06 -0300

    estilos en linea y core components

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, TextInput, View, Button } from "react-native";
 
 export default function App() {
+  const handleButton = () => {
+    console.log("Presione desde una función");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hola, Coder!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Ingresa aquí tu item"
+          style={{
+            borderBottomColor: "black",
+            borderBottomWidth: 2,
+            width: 200,
+          }}
+        />
+        <Button title="Agregar Item" onPress={handleButton} />
+      </View>
+      <View></View>
     </View>
   );
 }
